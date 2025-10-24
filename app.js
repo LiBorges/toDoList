df529a9dea54dfb4aac5728c254acf104346abdf
Lilia Borges <liliapds@gmail.com>
2025-10-24 16:22:52 -0300

    Criação do .env

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-/*
-CREATE TABLE `tarefa` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(100) NOT NULL,
-  `descricao` text NOT NULL,
-  `data` date NOT NULL,
-  `status` tinyint(1) NOT NULL
-)
-*/
 import http from 'http'; // Importa o módulo nativo HTTP do Node.js
 import express from 'express'; // Importa o framework Express para facilitar a criação do servidor
-import bodyParser from 'body-parser'; // Importa o body-parser para interpretar JSON no corpo das requisições
+import dotenv from 'dotenv'; // Importa o dotenv para carregar variáveis de ambiente
 import cors from 'cors'; // Importa o CORS para permitir requisições de outros domínios
 import tarefaRoutes from './routes/tarefasRoutes.js'; // Importa as rotas relacionadas às tarefas
 
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 const app = express(); // Cria uma instância do Express
-app.use(bodyParser.json()); // Configura o Express para aceitar JSON no corpo das requisições
+app.use(express.json()); // Habilita o parsing de JSON no corpo das requisições
 app.use(cors()); // Habilita o CORS para todas as rotas
 const port = 3000; // Define a porta onde o servidor irá rodar
 app.use(express.static('public')); // Serve arquivos estáticos da pasta public
